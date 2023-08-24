882a1fd6af469d06ce729e765f6b98dfeed1a16a
gustavo <gustavo.isaac@gmail.com>
2023-08-24 05:13:11 +0100

    Koin installation

--- src/main/kotlin/com/artemissoftware/Application.kt
+++ src/main/kotlin/com/artemissoftware/Application.kt
@@ -1,13 +1,18 @@
 package com.artemissoftware
 
+import com.artemissoftware.di.mainModule
 import com.artemissoftware.plugins.*
 import io.ktor.server.application.*
+import org.koin.ktor.plugin.Koin
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+    install(Koin) {
+        modules(mainModule)
+    }
     configureSockets()
     configureSerialization()
     configureMonitoring()
