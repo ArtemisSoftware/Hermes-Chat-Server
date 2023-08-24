8f331ddcab1d13c2c39c15c8785a58179eaca265
gustavo <gustavo.isaac@gmail.com>
2023-08-24 05:08:49 +0100

    mainModule

---
+++ src/main/kotlin/com/artemissoftware/di/MainModule.kt
@@ -0,0 +1,22 @@
+package com.artemissoftware.di
+
+import com.artemissoftware.data.MessageDataSource
+import com.artemissoftware.data.MessageDataSourceImpl
+import com.artemissoftware.room.RoomController
+import org.koin.dsl.module
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
+
+val mainModule = module {
+    single {
+        KMongo.createClient()
+            .coroutine
+            .getDatabase("message_db_yt")
+    }
+    single<MessageDataSource> {
+        MessageDataSourceImpl(get())
+    }
+    single {
+        RoomController(get())
+    }
+}
