b403051ec5d7dadddbcb1402b37d832ba8609b36
gustavo <gustavo.isaac@gmail.com>
2023-08-24 05:16:04 +0100

    Added routes to Routing

--- src/main/kotlin/com/artemissoftware/plugins/Routing.kt
+++ src/main/kotlin/com/artemissoftware/plugins/Routing.kt
@@ -1,10 +1,17 @@
 package com.artemissoftware.plugins
 
+import com.artemissoftware.room.RoomController
+import com.artemissoftware.routes.chatSocket
+import com.artemissoftware.routes.getAllMessages
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
 
 fun Application.configureRouting() {
-
-
+    val roomController by inject<RoomController>()
+    install(Routing) {
+        chatSocket(roomController)
+        getAllMessages(roomController)
+    }
 }
